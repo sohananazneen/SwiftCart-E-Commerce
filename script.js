3d73e6f31c397468130792f84dc0e509acc3fcb3
SohanaN <sohananaz01@gmail.com>
2026-02-17 04:36:14 +0600

    loading trending products

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("js connected");
+// Trending
+
+const loadTrending = () => {
+  const url = "https://fakestoreapi.com/products";
+  //   console.log(url);
+  fetch(url)
+    .then((res) => res.json())
+    // .then((data) => console.log(data));
+    .then((data) => displayTrendingProducts(data));
+};
+
+const displayTrendingProducts = (tendingP) => {
+  console.log(tendingP);
+};
+loadTrending();
