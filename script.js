94dc49c8b4b850f77d79fb4ed2a25ac20947bc16
SohanaN <sohananaz01@gmail.com>
2026-02-17 19:35:52 +0600

    products page

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Trending
+// Trending products
 
 const loadTrending = () => {
   const url = "https://fakestoreapi.com/products";
@@ -17,63 +17,125 @@ const displayTrendingProducts = (trendingP) => {
   trendingP.slice(0, 3).forEach((tProduct) => {
     // console.log(tProduct);
     const card = document.createElement("div");
-    card.innerHTML = `
-   
-  <div class="card bg-base-100 shadow-md hover:shadow-xl transition duration-300 h-full flex flex-col">
-    
-    <figure class="p-4 h-48 sm:h-56">
-      <img 
-        src="${tProduct.image}" 
-        alt="${tProduct.title}"
-        class="h-full w-full object-contain"
-      />
-    </figure>
-
-    <div class="card-body flex flex-col flex-grow p-4">
-
-      <div class="flex justify-between items-center mb-2 text-sm">
-        <span class="badge badge-primary text-xs">
-          ${tProduct.category}
-        </span>
-
-        <div class="flex items-center gap-1">
-        <div class="text-yellow-500">
-        <i class="fa-solid fa-star"></i>
-        </div>
-        <div>
-          <span>
-          ${tProduct.rating.rate}
-          </span>
-          <span>
-            (${tProduct.rating.count})
-          </span>
+    card.innerHTML = `   
+        <div class="card bg-base-100 shadow-md hover:shadow-xl transition duration-300 h-full flex flex-col">    
+          <figure class="p-4 h-48 sm:h-56">
+            <img 
+              src="${tProduct.image}" 
+              alt="${tProduct.title}"
+              class="h-full w-full object-contain"
+            />
+          </figure>
+          <div class="card-body flex flex-col flex-grow p-4">
+            <div class="flex justify-between items-center mb-2 text-sm">
+              <span class="badge badge-primary text-xs">
+                ${tProduct.category}
+              </span>
+              <div class="flex items-center gap-1">
+              <div class="text-yellow-500">
+              <i class="fa-solid fa-star"></i>
+              </div>
+              <div>
+                <span>
+                ${tProduct.rating.rate}
+                </span>
+                <span>
+                  (${tProduct.rating.count})
+                </span>
+                </div>
+              </div>
+            </div>
+            <h2 class="card-title text-sm md:text-base line-clamp-2 min-h-[48px]">
+              ${tProduct.title}
+            </h2>
+            <p class="text-lg font-bold text-primary mt-2">
+              $${tProduct.price}
+            </p>
+            <div class="card-actions mt-auto flex gap-3">  
+              <button class="btn btn-sm sm:btn-md btn-outline flex-1 w-full">
+                <i class="fa-regular fa-eye"></i>
+                <span class="ml-1">Details</span>
+              </button>
+              <button class="btn btn-sm sm:btn-md btn-primary flex-1 w-full">
+                <i class="fa-solid fa-cart-shopping"></i>
+                <span class="ml-1">Add</span>
+              </button>
+            </div>
           </div>
         </div>
-      </div>
-
-      <h2 class="card-title text-sm md:text-base line-clamp-2 min-h-[48px]">
-        ${tProduct.title}
-      </h2>
-
-      <p class="text-lg font-bold text-primary mt-2">
-        $${tProduct.price}
-      </p>
-
-      <div class="card-actions mt-auto flex gap-3">  
-        <button class="btn btn-sm sm:btn-md btn-outline flex-1 w-full">
-          <i class="fa-regular fa-eye"></i>
-          <span class="ml-1">Details</span>
-        </button>
-        <button class="btn btn-sm sm:btn-md btn-primary flex-1 w-full">
-          <i class="fa-solid fa-cart-shopping"></i>
-          <span class="ml-1">Add</span>
-        </button>
-      </div>
-    </div>
-  </div>
     `;
 
     tpContainer.append(card);
   });
 };
 loadTrending();
+
+const loadAllProducts = () => {
+  const url = "https://fakestoreapi.com/products";
+  // console.log(url);
+  fetch(url)
+    .then((res) => res.json())
+    // .then((data) => console.log(data))
+    .then((data) => displayAllProducts(data));
+};
+// for Product page
+const displayAllProducts = (allProduct) => {
+  // console.log(allProduct);
+  const productsContainer = document.getElementById("products-container");
+  productsContainer.innerHTML = "";
+
+  allProduct.forEach((allProducts) => {
+    // console.log(allProducts);
+    const card = document.createElement("div");
+    card.innerHTML = `   
+          <div class="card bg-base-100 shadow-md hover:shadow-xl transition duration-300 h-full flex flex-col">    
+            <figure class="p-4 h-48 sm:h-56">
+              <img 
+                src="${allProducts.image}" 
+                alt="${allProducts.title}"
+                class="h-full w-full object-contain"
+              />
+            </figure>
+            <div class="card-body flex flex-col flex-grow p-4">
+              <div class="flex justify-between items-center mb-2 text-sm">
+                <span class="badge badge-primary text-xs">
+                  ${allProducts.category}
+                </span>
+                <div class="flex items-center gap-1">
+                <div class="text-yellow-500">
+                <i class="fa-solid fa-star"></i>
+                </div>
+                <div>
+                  <span>
+                  ${allProducts.rating.rate}
+                  </span>
+                  <span>
+                    (${allProducts.rating.count})
+                  </span>
+                  </div>
+                </div>
+              </div>
+              <h2 class="card-title text-sm md:text-base line-clamp-2 min-h-[48px]">
+                ${allProducts.title}
+              </h2>
+              <p class="text-lg font-bold text-primary mt-2">
+                $${allProducts.price}
+              </p>
+              <div class="card-actions mt-auto flex gap-3">  
+                <button class="btn btn-sm sm:btn-md btn-outline flex-1 w-full">
+                  <i class="fa-regular fa-eye"></i>
+                  <span class="ml-1">Details</span>
+                </button>
+                <button class="btn btn-sm sm:btn-md btn-primary flex-1 w-full">
+                  <i class="fa-solid fa-cart-shopping"></i>
+                  <span class="ml-1">Add</span>
+                </button>
+              </div>
+            </div>
+          </div>
+    `;
+
+    productsContainer.append(card);
+  });
+};
+loadAllProducts();
